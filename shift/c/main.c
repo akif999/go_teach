e4fa96aa425efd2649c786cd9b00ac99592e3794
akif999 <aki01nf28@gmail.com>
2019-09-22 08:20:17 +0900

    Add new example (shift)

---
+++ shift/c/main.c
@@ -0,0 +1,17 @@
+// This is main.c
+
+#include <stdio.h>
+
+void main () {
+	unsigned char  a;
+	unsigned char  b;
+	unsigned short c;
+
+	a = 0x12;
+	b = 0x34;
+
+	c |= (unsigned short)(a << 8);
+	c |= (unsigned short)(b << 0);
+
+	printf("c is 0x%04X\n", c);
+}
---
+++ shift/wrong/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a uint8
+	var b uint8
+	var c uint16
+
+	a = 0x12
+	b = 0x34
+
+	c |= uint16(a << 8)
+	c |= uint16(b << 0)
+
+	fmt.Printf("c is 0x%04X\n", c)
+}
